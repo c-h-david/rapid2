8ad0bb57c4efab6e730a79d091fe1148cd236ae0
Cedric H. David <cedric.david@jpl.nasa.gov>
2024-09-19 15:56:17 -0700

    New file.

---
+++ README.md
@@ -0,0 +1,2 @@
+# RAPID2
+[![License (3-Clause BSD)](https://img.shields.io/badge/license-BSD%203--Clause-yellow.svg)](https://github.com/c-h-david/rapid2/blob/main/LICENSE)
---
+++ src/con_vec.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# *****************************************************************************
+# con_vec.py
+# *****************************************************************************
+
+# Author:
+# Cedric H. David, 2024-2024
+
+
+# *****************************************************************************
+# Import Python modules
+# *****************************************************************************
+import csv
+import numpy as np
+import numpy.typing as npt
+from typing import Tuple
+
+
+# *****************************************************************************
+# Connectivity function
+# *****************************************************************************
+def con_vec(
+            con_csv: str
+            ) -> Tuple[
+                       npt.NDArray[np.int32],
+                       npt.NDArray[np.int32]
+                       ]:
+    '''Read connectivity file.
+
+    Create two arrays of river IDs based on connectivity file.
+
+    Parameters
+    ----------
+    arg1 : str
+        Path the the connectivity file.
+
+    Returns
+    -------
+    ndarray[int32]
+        The river IDs of the domain.
+    ndarray[int32]
+        The river IDs downstream.
+
+    Examples
+    --------
+    >>> con_vec('../input/Test/rapid_connect_Test.csv')
+    (array([10, 20, 30, 40, 50], dtype=int32), array([30, 30, 50, 50,  0],\
+ dtype=int32))
+
+    '''
+
+    IV_riv_tot = np.empty(0, dtype=np.int32)
+    IV_dwn_tot = np.empty(0, dtype=np.int32)
+    with open(con_csv, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            IV_riv_tot = np.append(IV_riv_tot, np.int32(row[0]))
+            IV_dwn_tot = np.append(IV_dwn_tot, np.int32(row[1]))
+
+    return IV_riv_tot, IV_dwn_tot
+
+
+# *****************************************************************************
+# End
+# *****************************************************************************
