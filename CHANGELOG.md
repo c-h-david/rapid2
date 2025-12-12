5b2991995780e28a395b764dfe2c18817fac6d66
Cedric H. David <cedric.david@jpl.nasa.gov>
2025-12-12 10:14:42 -0800

    New file.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -63,6 +63,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0).
 | `./src/rapid2/cpl_vec.py`    | New file. |
 | `./src/rapid2/chk_cpl.py`    | New file. |
 | `./src/_cpllsm.py`           | New file. |
+| `./src/rapid2/inv_mat.py`    | New file. |
 
 ### Changed
 
---
+++ src/rapid2/inv_mat.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# *****************************************************************************
+# inv_mat.py
+# *****************************************************************************
+
+# Author:
+# Cedric H. David, 2025-2025
+
+
+# *****************************************************************************
+# Import Python modules
+# *****************************************************************************
+import numpy as np
+from scipy.sparse import (  # type: ignore[import-untyped]
+                          identity,
+                          csc_matrix,
+                          )
+
+
+# *****************************************************************************
+# Muskingum routing matrices
+# *****************************************************************************
+def inv_mat(
+            ZM_Net: csc_matrix,
+            ZM_C1m: csc_matrix,
+            ) -> csc_matrix:
+    '''Create inverse Muskingum matrix.
+
+    Create the inverse of the matrix used in matrix-based Muskingum method.
+
+    Parameters
+    ----------
+    ZM_Net : scipy.sparse.spmatrix
+        The network matrix for the basin.
+    ZM_C1m : scipy.sparse.spmatrix
+        The C1 parameter matrix for the basin.
+
+    Returns
+    -------
+    ZM_Inv : scipy.sparse.spmatrix
+        The inverse of the matrix used in matrix-based Muskingum method.
+
+    Examples
+    --------
+    >>> ZM_Net = csc_matrix(np.array([[0, 0, 0, 0, 0],\
+                                      [0, 0, 0, 0, 0],\
+                                      [1, 1, 0, 0, 0],\
+                                      [0, 0, 0, 0, 0],\
+                                      [0, 0, 1, 1, 0]]))
+    >>> ZM_C1m = csc_matrix(np.array([[-0.25,  0.  ,  0.  ,  0.  ,  0.  ],\
+                                      [ 0.  , -0.25,  0.  ,  0.  ,  0.  ],\
+                                      [ 0.  ,  0.  , -0.25,  0.  ,  0.  ],\
+                                      [ 0.  ,  0.  ,  0.  , -0.25,  0.  ],\
+                                      [ 0.  ,  0.  ,  0.  ,  0.  , -0.25]]))
+    >>> ZM_Inv = inv_mat(ZM_Net, ZM_C1m)
+    >>> ZM_Inv.toarray()
+    array([[ 1.    ,  0.    ,  0.    ,  0.    ,  0.    ],
+           [ 0.    ,  1.    ,  0.    ,  0.    ,  0.    ],
+           [-0.25  , -0.25  ,  1.    ,  0.    ,  0.    ],
+           [ 0.    ,  0.    ,  0.    ,  1.    ,  0.    ],
+           [ 0.0625,  0.0625, -0.25  , -0.25  ,  1.    ]])
+    '''
+
+    IS_riv_bas = ZM_Net.shape[0]
+    ZM_Idt = identity(IS_riv_bas, format='csc', dtype=np.float64)
+
+    ZM_C1N = ZM_C1m * ZM_Net
+
+    ZM_Inv = ZM_Idt
+    ZM_inc = ZM_C1N
+    for JS_riv_bas in range(IS_riv_bas):
+        ZM_Inv = ZM_Inv + ZM_inc
+        ZM_inc = ZM_inc * ZM_C1N
+
+    return ZM_Inv
+
+
+# *****************************************************************************
+# End
+# *****************************************************************************
